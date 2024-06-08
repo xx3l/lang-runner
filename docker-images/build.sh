ab64321299bd7bf5f0a1cbbc4a83758c643ccb7b
Alexei LEXX Zhukov <lexx@webmakerz.ru>
2024-06-08 05:56:48 +0000

    add Golang support

--- docker-images/build.sh
+++ docker-images/build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 cd `dirname "$0"`
-docker build -t lr-python ./python
-docker build -t lr-php ./php
-docker build -t lr-ruby ./ruby
-docker build -t lr-js ./js
 docker build -t lr-cpp ./cpp
+docker build -t lr-go ./go
+docker build -t lr-js ./js
+docker build -t lr-php ./php
+docker build -t lr-python ./python
+docker build -t lr-ruby ./ruby
